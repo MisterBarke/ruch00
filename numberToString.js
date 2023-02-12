f392bce81f3a5e8cd7d398d9fe08366b502e6009
Ousmane <bachirmahbarke@gmail.com>
2023-02-13 00:07:52 +0100

    work

--- numberToString.js
+++ numberToString.js
@@ -1,14 +1,18 @@
 
 
-function numberToString(myNumbers) {
-    const myString = {
-        1: "un", 2: "deux", 3: "trois", 4: "quatre", 5: "cinq", 6: "six", 7: "sept", 8: "huit", 9: "neuf",  10: "dix",
-    };
-    return myNumbers.map(number1 => myString[number1]);
+function numberToString(entiers) {
+    let noms = ["un", "deux", "trois", "quatre", "cinq", "six", "sept", "huit", "neuf", "dix"];
+    let resultat = [];
+
+    for (let i = 0; i < entiers.length; i++) {
+        if (entiers[i] >= 1 && entiers[i] <= 10) {
+            resultat.push(noms[entiers[i] - 1]);
+        }
+    }
+
+    return resultat;
 }
 
-const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const myString2 = numberToString(number);
-console.log(myString2);
-
+let entiers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+console.log(numberToString(entiers));
 module.exports = numberToString;
